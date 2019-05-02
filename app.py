341c920eb9819e3e393c61f8519d6efb89b3a54f
Henry <comprocho@gmail.com>
2019-05-02 10:22:11 -0400

    changes

--- app.py
+++ app.py
@@ -1,53 +1,61 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for 
+# from werkzeug.utils import secure_filename
 from ocr_core import ocr_core
 
 UPLOAD_FOLDER = 'static/uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# function to check the file extension
 def allowed_file(filename):
-	return "." in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# route and function to handle the homepage
-@app.route('/')
-def home_page():
-	return render_template('index.html')
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_page():
 	if request.method == 'POST':
+		# check if the post request has the file part
 		if 'file' not in request.files:
+			# flash('No file part')
 			return render_template('upload.html', msg='No file selected')
-		
 		file = request.files['file']
-
+		# if user does not select file, browser also
+        # submit an empty part without filename
 		if file.filename == '':
 			return render_template('upload.html', msg='No file selected')
-
 		if file and allowed_file(file.filename):
 			extracted_text = ocr_core(file)
-			print('{}'.format(file.filename))
+			# filename = secure_filename(file.filename)
+			filename = file.filename
+			# file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 			return render_template(
-				'upload.html', 
+				'upload.html',
 				msg='Successfully processed', 
 				extracted_text=extracted_text,
-				img_src=UPLOAD_FOLDER + file.filename
+				img_src=UPLOAD_FOLDER + filename,
+				# img_src=os.path.join(app.config['UPLOAD_FOLDER'],filename)
 			)
 	elif request.method == 'GET':
 		return render_template('upload.html')
 
-@app.route('/hello/<int:score>')
-def hello_name(score):
-	return render_template('hello.html', record=score)
+@app.route('/student')
+def student():
+	return render_template('student.html')
 
-dict = {'Physics':50, 'Math':60, 'Chemistry':100}
-
-@app.route('/result')
+@app.route('/result', methods = ['POST', 'GET'])
 def result():
-	return render_template('result.html', result = dict)
+	if request.method == 'POST':
+		result = request.form
+		return render_template('result.html', result=result)
+
+# route and function to handle the homepage
+# @app.route('/')
+# def home_page():
+# 	return render_template('index.html')
+# @app.route('/hello/<int:score>')
+# def hello_name(score):
+# 	return render_template('hello.html', record=score)
 
 if __name__ == '__main__':
 	app.run()
--- ocr_core.py
+++ ocr_core.py
@@ -6,10 +6,11 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 tessdata_dir_config = r'--tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata"'
-
+tess_config = tessdata_dir_config
+# tess_config = "-psm 7"
 def ocr_core(filename):
 	im = image_enhancer(filename)
-	text = pytesseract.image_to_string(im, lang='chi_sim', config=tessdata_dir_config)
+	text = pytesseract.image_to_string(im, lang='eng', config=tess_config)
 	return text
 
 def image_enhancer(filename):
@@ -17,5 +18,5 @@ def image_enhancer(filename):
 	im = im.filter(ImageFilter.MedianFilter())
 	enhancer = ImageEnhance.Contrast(im)
 	im = enhancer.enhance(2)
-	im = im.convert('1')
+	# im = im.convert('1')
 	return im
